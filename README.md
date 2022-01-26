e367d0cf913c93ea4c9c5d4b0d9b91ae329a36b8
Tadas Krivickas <tadas.krivickas@gmail.com>
2022-01-26 15:11:59 +0200

    Listen for HTTP calls, log get/post requests

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # webhook-logger
+
+Logs incoming REST calls to the console.
+
+## How to run
+
+```
+yarn
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express()
+app.get("/*", (req, res) => {
+    console.log("GET", req.path, new Date().toTimeString())
+    console.dir(req.body, { depth: null })
+    res.send()
+})
+app.post("/*", (req, res) => {
+    console.log("POST", req.path, new Date().toTimeString())
+    console.dir(req.body, { depth: null })
+    res.send()
+})
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
