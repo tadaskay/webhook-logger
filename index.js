ee9a456a6d495108944c0421d81f9e5578bbc9f6
Tadas Krivickas <tadas.krivickas@gmail.com>
2022-01-26 15:42:09 +0200

    Fix request payload logging

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
-
+const bodyParser = require("body-parser")
 const app = express()
+
+app.use(bodyParser.json())
+
 app.get("/*", (req, res) => {
     console.log("GET", req.path, new Date().toTimeString())
     console.dir(req.body, { depth: null })
